f3732f5fe2c125eb0b83ea02698ca9a547fb2207
Parviz <parviz.hajili@gmail.com>
2023-03-07 20:01:59 +0400

    Add project files.

---
+++ introduction.js
@@ -0,0 +1,16 @@
+//console.log("Introduction Js")
+
+console.log(typeof "Notebook");
+console.log(typeof 5);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+
+let firstName = "Parviz",
+  age = 23,
+  isMarried = false,
+  monthlySalary = undefined;
+const gravity = 9.81;
+const PI = 3.14;
+
+console.log(firstName, age, isMarried, monthlySalary, gravity, PI);
